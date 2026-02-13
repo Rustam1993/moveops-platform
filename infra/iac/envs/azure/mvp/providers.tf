a7f7129dbdfd61dbcfbfec7b46acb909b2a7b4ba
Rustam <rustam@Rustams-MacBook-Pro.local>
2026-02-13 11:51:39 -0800

    IaC: disable azurerm provider auto-registration

--- infra/iac/envs/azure/mvp/providers.tf
+++ infra/iac/envs/azure/mvp/providers.tf
@@ -1,3 +1,6 @@
 provider "azurerm" {
   features {}
+  # GitHub OIDC principal doesn't have permissions to register arbitrary resource providers at subscription scope.
+  # Providers are pre-registered for this MVP (see Phase 7 prereqs).
+  resource_provider_registrations = "none"
 }
